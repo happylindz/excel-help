9b40a6c6680c714eb2e37f1055f0c61bb5058bf3
happylindz <lindongzhou95@gmail.com>
2017-03-26 11:57:57 +0800

    update

--- public/js/main.js
+++ public/js/main.js
@@ -52,12 +52,22 @@ $(document).ready(function(){
 				  type: "success",
 				  confirmButtonText: "下一步",
 					allowOutsideClick: false
-				}).then(function() {
-						$("#excel_input").fileinput("reset");
-						$("#excel_input").val("");
-					}).catch(function(err) {
-						console.log(err);
-					});
+				});
+				// .then(function() {
+				// 	// 收表地址：${downloadAddress}<br>通过上述网址获取填表结果。
+				// 	var message = `至：<div class="social-share" data-sites="qq,wechat" data-description="请把收表地址收藏到你的微信上" data-wechat-qrcode-title="扫一扫, 收藏填表地址"></div>`;
+				// 	swal({
+				// 		title: '确认收表地址',
+				// 		html: message,
+				// 		type: 'success',
+				// 		confirmButtonText: "确认",
+				// 		allowOutsideClick: false
+				// 	}).then(function() {
+				// 		$("#excel_input").fileinput("reset");
+				// 		$("#excel_input").val("");
+				// 	}).catch(function(err) {
+				// 		console.log(err);
+				// 	});
 					// var $config = {
 				 //  	url: downloadAddress,
 				 //  	title: "IForm 我收表",
@@ -68,16 +78,7 @@ $(document).ready(function(){
 				}).catch(function(err) {
 					console.log(err);
 				})
-				// then(function() {
-				// 	// 收表地址：${downloadAddress}<br>通过上述网址获取填表结果。
-				// 	var message = `<div class="social-share" data-sites="qq,wechat" data-description="请把收表地址收藏到你的微信上" data-wechat-qrcode-title="扫一扫, 收藏填表地址"></div>`;
-				// 	swal({
-				// 		title: '确认收表地址',
-				// 		html: message,
-				// 		type: 'success',
-				// 		confirmButtonText: "确认",
-				// 		allowOutsideClick: false
-				// 	})
+
 				var $config = {
 			  	url: shareAddress,
 			  	title: "IForm 我收表",
